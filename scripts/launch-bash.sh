878e29b0ffdeaca368d1228d3e8ceebe0e8cbae0
gcornut <gcornut@gmail.com>
2017-02-20 19:01:45 +0100

    Add postgresql conf increasing maximum number of connections. Move tomcat container config to the docker-compose file and remove the tomcat Dockerfile. Other minor improvements.

--- scripts/launch-bash.sh
+++ scripts/launch-bash.sh
@@ -4,14 +4,20 @@
 MINE_NAME=$1
 [ -z "$MINE_NAME" ] && {
   echo "Missing script argument. Exiting."
-  echo -e "\nUsage: $0 <mine name>"
+  echo -e "\nUsage: $0 <mine name> [<conainter type>]"
   echo -e "\t<mine name>: name of the intermine container you want to reach"
+  echo -e "\t<container type>: ('main' by default) caontiner types: main, postgres, tomcat"
   exit 1
 }
+CONTAINER_TYPE=main
+[ -n "$2" ] && CONTAINER_TYPE=$2
 
-MINE_CONTAINER_ID=$(docker ps | grep ${MINE_NAME}_main | cut -f1 -d$' ')
+SHELL=bash
+[ -n "$3" ] && SHELL=$3
+
+MINE_CONTAINER_ID=$(docker ps | grep ${MINE_NAME}_${CONTAINER_TYPE} | cut -f1 -d$' ')
 [ -z "$MINE_CONTAINER_ID" ] && {
   echo "No $MINE_NAME intermine container running. Exiting."
   exit 1
 }
-docker exec -it $MINE_CONTAINER_ID bash
+docker exec -it $MINE_CONTAINER_ID $SHELL
